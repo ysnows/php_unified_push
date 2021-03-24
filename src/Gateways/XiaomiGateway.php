7cd00c89fb112cc379054378c83538bbd7d5c087
咸光金 <yong531315@gmail.com>
2021-03-24 10:44:37 +0800

    添加统一推送

--- src/Gateways/XiaomiGateway.php
+++ src/Gateways/XiaomiGateway.php
@@ -33,6 +33,7 @@ class XiaomiGateway extends Gateway
         parent::__construct($config);
         Constants::setPackage($this->config->get("appPkgName"));
         Constants::setSecret($this->config->get("appSecret"));
+
         $this->sender = new Sender();
 
 
@@ -123,8 +124,9 @@ class XiaomiGateway extends Gateway
         $msg->title($message->title);
         $msg->description($message->subTitle);
         $msg->passThrough(0);
-        $msg->notifyType($message->notifyType);
-        $msg->extra(Builder::soundUri, $message->soundUrl);
+        $msg->notifyType(intval($message->notifyType));
+        $msg->extra(Builder::soundUri, 'android.resource://' . Constants::$packageName . '/raw/' . trim($message->soundUrl));
+
         $msg->payload(json_encode($message->payload)); // 对于预定义点击行为，payload会通过点击进入的界面的intent中的extra字段获取，而不会调用到onReceiveMessage方法。
 //        $msg->extra(Builder::notifyEffect, 1); // 此处设置预定义点击行为，1为打开app
         $msg->extra(Builder::notifyForeground, 1);
